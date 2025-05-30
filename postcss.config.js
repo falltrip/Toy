cfc66c64f934ea3c8dd7f37a45e38926106bc77f
falltrip <gunny97@naver.com>
2025-05-30 21:52:46 +0900

    com

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 // postcss.config.js
-module.exports = {
+// 기존: module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+// 변경 후:
+export default {
   plugins: {
-    tailwindcss: {}, // Tailwind CSS 3.x 이상부터는 이 형태를 권장합니다.
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
