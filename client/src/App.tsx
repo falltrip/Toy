511d2469a7eee2878a9d7c6458c8593b02ca5b20
Wo <129751052+falltrip@users.noreply.github.com>
2025-05-30 14:26:29 +0000

    Deploy to gh-pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,7 +8,7 @@ import Home from "@/pages/Home";
 import NewNavigation from "@/components/NewNavigation";
 import Footer from "@/components/Footer";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -25,7 +25,9 @@ function App() {
         <div className="min-h-screen flex flex-col">
           <NewNavigation />
           <div className="flex-grow">
-            <Router />
+            <Router base="/Toy">
+              <AppRouter />
+            </Router>
           </div>
           <Footer />
         </div>
